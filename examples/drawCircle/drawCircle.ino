3bd301813a27d16d9a75b665c5ebbaaab7dee5ff
Wolfgang Keller <nubok@users.noreply.github.com>
2014-11-06 22:02:50 +0100

    Update drawCircle.ino
    Removed a space for homogenous comment style.

--- examples/drawCircle/drawCircle.ino
+++ examples/drawCircle/drawCircle.ino
@@ -15,7 +15,7 @@ void setup()
 
     Tft.drawCircle(100, 100, 30,YELLOW);                //center: (100, 100), r = 30 ,color : YELLOW              
     
-    Tft.drawCircle(100, 200, 40,CYAN);                  // center: (100, 200), r = 10 ,color : CYAN  
+    Tft.drawCircle(100, 200, 40,CYAN);                  //center: (100, 200), r = 10 ,color : CYAN  
     
     Tft.fillCircle(200, 100, 30,RED);                   //center: (200, 100), r = 30 ,color : RED    
     
